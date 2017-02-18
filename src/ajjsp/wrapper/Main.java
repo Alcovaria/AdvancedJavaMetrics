f810a8a86793424c5769b65d664854ae60c165f2
Vyacheslav Bikbaev <la.sintez@gmail.com>
2017-02-18 18:39:23 +0300

    Renamed tests, added tests for FOR, WHILE, IF statements.

--- src/ajjsp/wrapper/Main.java
+++ src/ajjsp/wrapper/Main.java
@@ -1,8 +1,6 @@
 package ajjsp.wrapper;
 
-import com.digiarea.es5.FunctionDeclaration;
-import com.digiarea.es5.Node;
-import com.digiarea.es5.VariableDeclaration;
+import com.digiarea.es5.*;
 import com.digiarea.es5.parser.*;
 
 import java.io.ByteArrayInputStream;
@@ -12,17 +10,12 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws ParseException {
-        String code = "a = 10; b = 15;";
-
+        String code = "for (var prop in obj) {\n" +
+                "   console.log('obj.' + prop, '=', obj[prop]);\n" +
+                "}";
         InputStream stream = new ByteArrayInputStream(code.getBytes(StandardCharsets.UTF_8));
-
         ASTParser parser = new ASTParser(stream);
-
-        List<VariableDeclaration> var = parser.VariableDeclaratorList();
-        for (VariableDeclaration aVar : var) {
-            System.out.println(aVar.toString());
-
-        }
-
+        ForeachStatement forins = parser.ForInVarStatement();
+        System.out.println(forins.getVariable().toString());
     }
 }
